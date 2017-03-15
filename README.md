e1c4aad801d6c1befa197642b1c3e847edc92636
Le Hoang Duc <d.hoang@rainbow-software.vn>
2017-03-15 17:21:45 +0700

    Init version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# wl-bootstrap
-Use any functions, methods, libraries of Laravel in WordPress project
+WordPress Laravel Bootstrap
+======
+
+> Use any functions, methods, libraries of Laravel in any WordPress projects
+
+## Requiments
+
+- Laravel >= 5.2
+- WordPress >= 4.6
+
+## Installation
+
+- Copy "wl-bootstrap" folder into "wp-content/plugins"
+- Define Laravel source code path in "wp-config.php" as a constant:
+
+```
+define('LARAVEL_PATH', '/srv/www/laravel-example-project'); // Make sure this is ponited to same server
+```
+
+## Usage
+
+You can use all codes in your Laravel project, build Single Sign On with less effort on same/subdomain and so on... 
+
+```php
+<?php
+
+// Helper
+$array = array_add(['name' => 'Desk'], 'price', 100);
+
+// Session
+session(['chairs' => 7, 'instruments' => 3]);
+
+// Authentication
+Auth::check();
+
+// Query Builder
+$users = DB::table('users')->get();
+
+// Eloquent
+$flights = App\Flight::where('active', 1)
+               ->orderBy('name', 'desc')
+               ->take(10)
+               ->get();
+
+```
+
+## Contributing
+
+All contributions are welcome to help improve WL-Bootstrap.
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2017-present, Golr
---
+++ wl-bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package WL_Bootstrap
+ * @version 1.0
+ */
+/*
+Plugin Name: WL_Bootstrap
+Description: This plugin allows you to use any functions, methods, libraries of Laravel in WordPress project
+Author: Duc Le
+Version: 1.0
+Author URI: https://engineering.golr.xyz
+*/
+
+function wl_bootstrap() {
+    if (!defined('LARAVEL_PATH')) {
+        throw new Exception('LARAVEL_PATH is not configured.');
+    }
+
+    require LARAVEL_PATH . '/bootstrap/autoload.php';
+
+    $app = require_once LARAVEL_PATH . '/bootstrap/app.php';
+
+    $kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
+    $request = \Illuminate\Http\Request::capture();
+
+    $app->instance('request', $request);
+    $kernel->bootstrap();
+
+    $response = (new \Illuminate\Routing\Pipeline($app))
+        ->send($request)
+        ->through([
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class
+        ])
+        ->then(function () {
+            return Response::instance();
+        });
+
+    // Set cookie from response headers
+    foreach ($response->headers->getCookies() as $cookie) {
+        if ($cookie->isRaw()) {
+            setrawcookie($cookie->getName(), $cookie->getValue(), $cookie->getExpiresTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly());
+        } else {
+            setcookie($cookie->getName(), $cookie->getValue(), $cookie->getExpiresTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly());
+        }
+    }
+}
+
+add_action('wp', 'wl_bootstrap');
